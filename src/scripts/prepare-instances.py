aa4f96e0a4ba6fe4dfe9f154be3e10b5edac9a20
Steffen <bigsteff987@aol.com>
2020-06-28 10:54:55 -0700

    fixed bugs

--- src/scripts/prepare-instances.py
+++ src/scripts/prepare-instances.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 import boto3
 from botocore.exceptions import ClientError
 
@@ -25,19 +27,31 @@ def create_benchmark_instance(ec2_resource, ami_id, key_name, secgroup_id):
 def prepare_osv_benchmark_instance(ec2_client, ec2_resource, key_name, secgroup_id):
     resp_ami = ec2_client.describe_images(Filters=[{'Name': 'name', 'Values': ['OSv Image']}], Owners=['self'])
     ami_id = resp_ami['Images'][0]['ImageId']
+
     instance_id = create_benchmark_instance(ec2_resource, ami_id, key_name, secgroup_id)
-    
     instance = ec2_resource.Instance(id=instance_id)
     instance.wait_until_running()
     instance.stop()
+
     return instance_id
 
-def prepare_linux_benchmark_instance(ec2_client):
+def prepare_linux_benchmark_instance(ec2_client, ec2_resource, key_name, key_material, secgroup_id):
     resp_ami = ec2_client.describe_images(Filters=[{'Name': 'description', 'Values': ['Amazon Linux 2 LTS Candidate AMI 2017.12.0.20171212.2 x86_64 HVM EBS']}], Owners=['amazon'])
     ami_id = resp_ami['Images'][0]['ImageId']
-    print(ami_id)
-    #ami_id = resp_ami['Images']['ImageId']
-    #create_benchmark_instance(ec2_client, ami_id, key_name, secgroup_id)
+    
+    instance_id = create_benchmark_instance(ec2_resource, ami_id, key_name, secgroup_id)
+    instance = ec2_resource.Instance(id=instance_id)
+    instance.wait_until_running()
+
+    #Connecting to instance
+    with open('keypair.pem', 'w') as f:
+        f.write(key_material)
+        f.close()
+    os.chmod('keypair.pem', 444)
+
+    #Stopping instance
+    instance.stop()
+    
 
 ec2_client = boto3.client('ec2')
 ec2_resource = boto3.resource('ec2')
@@ -45,10 +59,8 @@ ec2_resource = boto3.resource('ec2')
 try:
     key_name, key_material = create_benchmark_keypair(ec2_client)
     secgroup_id = create_and_authorize_benchmark_security_group(ec2_client)
-    #prepare_linux_benchmark_instance(ec2_client)
     prepare_osv_benchmark_instance(ec2_client, ec2_resource, key_name, secgroup_id)
-    #prepare_linux_benchmark_instance(ec2_client, key_name, key_material, secgroup_id)
-    #prepare_osv_benchmark_instance(ec2_client, key_name, secgroup_id)
+    prepare_linux_benchmark_instance(ec2_client, ec2_resource, key_name, key_material, secgroup_id)
     
 
     

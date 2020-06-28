708a7695d13c5ce87f335f3d1fdb0c65196aefa6
Steffen <bigsteff987@aol.com>
2020-06-28 04:50:25 -0700

    fixed bugs & prepare-benchmark.sh working

--- scripts/prepare-benchmark.sh
+++ scripts/prepare-benchmark.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd src
-docker build -t unikernel-benchmark .
-docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark ./upload-osvimage-s3.sh
-docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark ./create-vmimport-role.sh
-docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark ./import-osvimage-snapshot.sh
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+echo $AWS_DEFAULT_REGION
+
+cd ../src
+sudo docker build -t unikernel-benchmark .
+sudo docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark ./upload-osvimage-s3.sh
+sudo docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark ./create-vmimport-role.sh
+sudo docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark ./import-osvimage-snapshot.sh
--- src/scripts/create-vmimport-role.sh
+++ src/scripts/create-vmimport-role.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /usr/src/config
+cd /usr/src/aws/config
 aws iam create-role --role-name vmimport --assume-role-policy-document "file://trust-policy.json"
 aws iam put-role-policy --role-name vmimport --policy-name vmimport --policy-document "file://role-policy.json"

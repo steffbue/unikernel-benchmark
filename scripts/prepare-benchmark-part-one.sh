af475194f4ba280394392a8c14fe4a7ad59aaa52
Steffen <bigsteff987@aol.com>
2020-07-01 03:34:31 -0700

    continued finishing scripts

--- scripts/prepare-benchmark-part-one.sh
+++ scripts/prepare-benchmark-part-one.sh
@@ -2,5 +2,6 @@
 
 cd ../src
 sudo docker build -t unikernel-benchmark .
+sudo docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark python3 create-control-server-ip.py
 sudo docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark ./create-vmimport-role.sh
 sudo docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark python3 prepare-resources.py

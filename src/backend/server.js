0ddb435e1d2a259b7688f2ccec0b383326ec350d
Steffen <bigsteff987@aol.com>
2020-07-02 08:30:51 -0700

    added logic for boot time & execution time measurement in backend

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,12 +1,32 @@
-var express = require('express');
+const fs = require('fs')
+const axios = require('axios')
+const express = require('express');
+const app = express();
 
-console.log('Hello World');
+const SEC_TO_MS = 1e3;
+const NS_TO_MS = 1e-6;
 
-var app = express();
 
-app.get('/', (req, res) => {
-	console.log('Hello World');
-	res.end('Hello World');
+ipAddr = fs.readFileSync('ip-info.txt')
+
+axios.put(`http://${ipAddr}:80/metric/boot`)
+	.catch(error => {
+		console.log(error)
+	})
+
+
+
+app.get('/metric/execution', (req, res) => {
+	startTime = process.hrtime()
+	// Executing task
+	diffTime = process.hrtime(startTime)
+
+	resJson = {
+		ExecutionTime: (diffTime[0] * SEC_TO_MS) + (diffTime[1] * NS_TO_MS) 
+	}
+
+	res.json(resJson)
+	res.end()
 });
 
-app.listen(8080);
+app.listen(80);

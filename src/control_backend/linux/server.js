69b0de5a8d50304bf343fdc83cd2180280e48622
Steffen <bigsteff987@aol.com>
2020-07-03 07:03:41 -0700

    finished boot time measure backend

--- src/control_backend/linux/server.js
+++ src/control_backend/linux/server.js
@@ -2,13 +2,16 @@ const express = require('express')
 const AWS = require('aws-sdk');
 const app = express();
 
+const SEC_TO_MS = 1e3;
+const NS_TO_MS = 1e-6;
+
 var ec2 = new AWS.EC2();
 const filter = {Name: 'tag:Benchmark', Values: ['Unikernel']};
-const filter_linux_instance = [filter, {Name: 'tag:Type', Values: ['Linux']}];
+const filterLinuxInstance = [filter, {Name: 'tag:Type', Values: ['Linux']}];
 
 
 var instanceIdLinux;
-ec2.describeInstances({Filters: filter_linux_instance}, function(err, data){
+ec2.describeInstances({Filters: filterLinuxInstance}, function(err, data){
     if (err) throw err;
     instanceIdLinux = data[0].Instances[0].InstanceId; 
 });
@@ -16,32 +19,55 @@ ec2.describeInstances({Filters: filter_linux_instance}, function(err, data){
 var startTime;
 var diffTime;
 
+var serviceReady = true;
+var resultReady = false;
+
 app.put('/metric/boot/start', (req, res) => {
-    ec2.startInstances({InstanceIds: [instanceIdLinux]}, function(err, data){
-        if (err) {
-            res.writeHead(500);
-            res.end();
-        }
-        startTime = process.hrtime();
-        res.writeHead(200)
+    if(!serviceReady) {
+        res.writeHead(503);
         res.end();
-    }); 
+    } else {
+        ec2.startInstances({InstanceIds: [instanceIdLinux]}, function(err, data){
+            if (err) {
+                res.writeHead(500);
+                res.end();
+            }
+            startTime = process.hrtime();
+            res.writeHead(200);
+
+            serviceReady = false;
+
+            res.end();
+        });
+    }
+    
 });
 
 
 app.get('/metric/boot/result', (req, res) => {
+    if(!resultReady) {
+        res.writeHead(404);
+        res.end();
+    } else {
+        res.writeHead(200);
+        res.json({BootTime: (diffTime[0] * SEC_TO_MS) + (diffTime[1] * NS_TO_MS)});
 
+        serviceReady = true;
+        resultReady = false;
+
+        res.end();
+    }
 });
 
 app.put('/metric/boot', (req, res) => {
-    diffTime = process.hrtime(startTime);
-    //TODO Condition
     ec2.stopInstances({InstanceIds: [instanceIdLinux]}, function(err, data){
         if (err) throw err;
-        startTime = process.hrtime();
-    });
-
-    res.end();
+        ec2.waitFor('instanceStopped', {Filters: filterLinuxInstance}, function(err, data) {
+            diffTime = process.hrtime(startTime);
+            resultReady = true;
+            res.end();
+        });
+    });  
 });
 
 app.listen(8080)
--- src/control_backend/osv/server.js
+++ src/control_backend/osv/server.js
@@ -2,13 +2,16 @@ const express = require('express')
 const AWS = require('aws-sdk');
 const app = express();
 
+const SEC_TO_MS = 1e3;
+const NS_TO_MS = 1e-6;
+
 var ec2 = new AWS.EC2();
 const filter = {Name: 'tag:Benchmark', Values: ['Unikernel']};
-const filter_osv_instance = [filter, {Name: 'tag:Type', Values: ['OSV']}];
+const filterOSVInstance = [filter, {Name: 'tag:Type', Values: ['OSV']}];
 
 
 var instanceIdOSV;
-ec2.describeInstances({Filters: filter_osv_instance}, function(err, data){
+ec2.describeInstances({Filters: filterOSVInstance}, function(err, data){
     if (err) throw err;
     instanceIdOSV = data[0].Instances[0].InstanceId; 
 });
@@ -16,32 +19,55 @@ ec2.describeInstances({Filters: filter_osv_instance}, function(err, data){
 var startTime;
 var diffTime;
 
-app.put('/metric/boot/osv/start', (req, res) => {
-    ec2.startInstances({InstanceIds: [instanceIdOSV]}, function(err, data){
-        if (err) {
-            res.writeHead(500);
-            res.end();
-        }
-        startTime = process.hrtime();
-        res.writeHead(200)
+var serviceReady = true;
+var resultReady = false;
+
+app.put('/metric/boot/start', (req, res) => {
+    if(!serviceReady) {
+        res.writeHead(503);
         res.end();
-    });
+    } else {
+        ec2.startInstances({InstanceIds: [instanceIdOSV]}, function(err, data){
+            if (err) {
+                res.writeHead(500);
+                res.end();
+            }
+            startTime = process.hrtime();
+            res.writeHead(200);
+
+            serviceReady = false;
+
+            res.end();
+        });
+    }
+    
 });
 
 
 app.get('/metric/boot/result', (req, res) => {
+    if(!resultReady) {
+        res.writeHead(404);
+        res.end();
+    } else {
+        res.writeHead(200);
+        res.json({BootTime: (diffTime[0] * SEC_TO_MS) + (diffTime[1] * NS_TO_MS)});
 
+        serviceReady = true;
+        resultReady = false;
+
+        res.end();
+    }
 });
 
 app.put('/metric/boot', (req, res) => {
-    diffTime = process.hrtime(startTime);
-    //TODO Condition
     ec2.stopInstances({InstanceIds: [instanceIdOSV]}, function(err, data){
         if (err) throw err;
-        startTime = process.hrtime();
-    });
-
-    res.end();
+        ec2.waitFor('instanceStopped', {Filters: filterOSVInstance}, function(err, data) {
+            diffTime = process.hrtime(startTime);
+            resultReady = true;
+            res.end();
+        });
+    });  
 });
 
 app.listen(8080)

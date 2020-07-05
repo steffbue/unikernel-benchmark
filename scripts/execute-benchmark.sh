fc28968faf5284dfc91346f695118387f2a885ba
Steffen Büschking <bigsteff987@aol.com>
2020-07-05 13:37:50 +0200

    added venv folder to gitignore

---
+++ scripts/execute-benchmark.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../src
+sudo docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION unikernel-benchmark python3 execute-benchmark.py
---
+++ src/scripts/execute-benchmark.py
@@ -0,0 +1,4 @@
+import boto3
+import boto3.session
+from botocore.exceptions import ClientError
+
